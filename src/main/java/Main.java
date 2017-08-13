a9d85ac5383b4b28d68ad0fbeae5e57010564780
runhwguo <444347292@qq.com>
2017-08-13 10:14:57 +0800

    增加对文件的加解密

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,11 +1,58 @@
-import org.apache.commons.codec.DecoderException;
+import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) throws DecoderException {
-        String passwordRandom = Util.getPasswordRandomLen();
-        System.out.println("随机生成的密码：\n"+passwordRandom);
-        String encrypt = Util.encode(passwordRandom);
-        System.out.println("加密后可传输存储的密码：\n"+encrypt);
-        System.out.println("解密后的密码：\n"+Util.decode(encrypt));
+    public static void main(String[] args) throws Exception {
+        Util util = Util.getInstance();
+
+        String menu = "1.随机生成密码\n" +
+                "2.随机生成指定长度密码\n" +
+                "3.对称加密字符串\n" +
+                "4.对称解密字符串\n" +
+                "5.对称加密文件目录\n" +
+                "6.对称解密文件目录\n" +
+                "7.退出\n";
+        System.out.println(menu);
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("请选择操作项：");
+        int menuNum = scanner.nextInt();
+        scanner.nextLine();
+        String securityKey;
+        if (menuNum >= 3 && menuNum <= 6) {
+            System.out.println("请输入密钥：");
+            securityKey = scanner.nextLine();
+            util.setKey(securityKey);
+        }
+        switch (menuNum) {
+            case 1:
+                System.out.println(util.getPasswordRandomLen());
+                break;
+            case 2:
+                System.out.println(util.getPassword(scanner.nextInt()));
+                break;
+            case 3:
+                System.out.println("请输入字符串：");
+                System.out.println(util.encode(scanner.nextLine()));
+                break;
+            case 4:
+                System.out.println("请输入字符串：");
+                System.out.println(util.decode(scanner.nextLine()));
+                break;
+            case 5:
+                System.out.println("请输入文件目录：");
+                util.encode(scanner.nextLine(), true);
+                break;
+            case 6:
+                System.out.println("请输入文件目录：");
+                util.decode(scanner.nextLine(), true);
+                break;
+            case 7:
+                break;
+            default:
+                System.out.println("没有该选项");
+                break;
+        }
+
+        scanner.close();
     }
 }
--- src/main/java/Util.java
+++ src/main/java/Util.java
@@ -1,19 +1,45 @@
 import org.apache.commons.codec.binary.Base64;
+import org.apache.commons.io.FileUtils;
 
+import java.io.File;
 import java.nio.charset.StandardCharsets;
+import java.util.Collection;
 import java.util.Random;
 
 public class Util {
-    public static final String KEY = "YOUR KEY";
+    private static String key;
 
-    private static final Random random = new Random();
+    private static Util instance;
+
+    private Util() {
+    }
+
+    public static Util getInstance() {
+        if (instance == null) {
+            instance = new Util();
+        }
+        return instance;
+    }
+
+    public void setKey(String k) {
+        key = k;
+    }
+
+    private final Random random = new Random();
+
+    private final String[] LEGAL_EXTENSION = new String[]{"js"};
+
+    private enum SecurityOperation {
+        ENCODE,
+        DECODE
+    }
 
 
-    public static String getPassword(int len) {
+    public String getPassword(int len) {
         int index;  //生成的随机数
         int count = 0; //生成的密码的长度
         // 密码字典
-        char[] str = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789~!@#$%^&*()_+-=[]\\{}|;':\",./<>?".toCharArray();
+        char[] str = "ABCDEFGHIJKLMNPQRSTUVWXYZabcdefghijkmnpqrstuvwxyz23456789~!@#$%^&*()_+-=[]\\{}|;':\",./<>?".toCharArray();
         String password = "";
         while (count < len) {
             //生成 0 ~ 密码字典-1之间的随机数
@@ -24,22 +50,56 @@ public class Util {
         return password;
     }
 
-    public static String getPasswordRandomLen() {
+    public String getPasswordRandomLen() {
         int max = 20,
                 min = 10;
         int len = random.nextInt(max) % (max - min + 1) + min;
         return getPassword(len);
     }
 
-    public static String encode(String s) {
-        return Base64.encodeBase64String(xorWithKey(s.getBytes(StandardCharsets.UTF_8), KEY.getBytes(StandardCharsets.UTF_8)));
+    public String encode(String str) {
+        return Base64.encodeBase64String(xorWithKey(str.getBytes(StandardCharsets.UTF_8), key.getBytes(StandardCharsets.UTF_8)));
     }
 
-    public static String decode(String s) {
-        return new String(xorWithKey(Base64.decodeBase64(s), KEY.getBytes(StandardCharsets.UTF_8)));
+    public String decode(String str) {
+        return new String(xorWithKey(Base64.decodeBase64(str), key.getBytes(StandardCharsets.UTF_8)));
     }
 
-    private static byte[] xorWithKey(byte[] a, byte[] key) {
+    private void directorySecurityOperation(String dirStr, boolean cover, SecurityOperation operation) throws Exception {
+        File dir = FileUtils.getFile(dirStr);
+        Collection<File> files = FileUtils.listFiles(dir, LEGAL_EXTENSION, true);
+        for (File f : files) {
+            String fileString = FileUtils.readFileToString(f, StandardCharsets.UTF_8);
+            String transformString;
+            switch (operation) {
+                case DECODE:
+                    transformString = decode(fileString);
+                    break;
+                case ENCODE:
+                    transformString = encode(fileString);
+                    break;
+                default:
+                    throw new Exception("not have this security operation");
+            }
+            if (cover) {
+                FileUtils.writeStringToFile(f, transformString, StandardCharsets.UTF_8);
+            } else {
+                String bakFilePath = f.getAbsolutePath() + ".bak";
+                File bakFile = FileUtils.getFile(bakFilePath);
+                FileUtils.writeStringToFile(bakFile, transformString, StandardCharsets.UTF_8);
+            }
+        }
+    }
+
+    public void decode(String dirStr, boolean cover) throws Exception {
+        directorySecurityOperation(dirStr, cover, SecurityOperation.DECODE);
+    }
+
+    public void encode(String dirStr, boolean cover) throws Exception {
+        directorySecurityOperation(dirStr, cover, SecurityOperation.ENCODE);
+    }
+
+    private byte[] xorWithKey(byte[] a, byte[] key) {
         byte[] out = new byte[a.length];
         for (int i = 0; i < a.length; i++) {
             out[i] = (byte) (a[i] ^ key[i % key.length]);
